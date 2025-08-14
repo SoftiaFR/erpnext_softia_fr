043c761c503027f2cf02b0ac862beba3ffa80df7
aratsimba <aratsimba@softia.fr>
2025-08-14 15:05:01 +0300

    Ajustement du fichier Opportunity.js

--- erpnext_softia_fr/public/js/opportunity.js
+++ erpnext_softia_fr/public/js/opportunity.js
@@ -1,12 +1,12 @@
-frappe.ui.form.on('Opportunity', {
-    setup: function(frm) {
-        frm.set_df_property('opportunity_type', 'default', 'Ventes');
-    },
-    
+frappe.ui.form.on('Opportunity', { 
     refresh: function(frm) {
         if (frm.is_new()) {
-            if (!frm.doc.opportunity_type || frm.doc.opportunity_type === 'Sales') {
-                frm.set_value('opportunity_type', 'Ventes');
+            let default_value = frappe.boot.lang === 'fr' ? 'Ventes' : 'Sales';
+            
+            if (!frm.doc.opportunity_type || 
+                (frappe.boot.lang === 'fr' && frm.doc.opportunity_type === 'Sales') ||
+                (frappe.boot.lang !== 'fr' && frm.doc.opportunity_type === 'Ventes')) {
+                frm.set_value('opportunity_type', default_value);
             }
         }
     }
